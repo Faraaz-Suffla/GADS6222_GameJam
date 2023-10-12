eef02daa4842cac60554db37b550a7ac1ae069f1
JonathanCDavids <110395998+JonathanCDavids@users.noreply.github.com>
2023-10-12 14:12:52 +0200

    add test

---
+++ Assets/jon.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class jon : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        // hi hi hi hi
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
