40cb8ea592849768b91052fdf656366063085e18
Faraaz Suffla <110396388+Faraaz-Suffla@users.noreply.github.com>
2023-10-28 17:55:59 +0200

    updates

--- Assets/Scripts/PuzzleManager.cs
+++ Assets/Scripts/PuzzleManager.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class PuzzleManager : MonoBehaviour
 {
     [SerializeField] private List<PuzzleSlot> _slotPrefabs;
-    [SerializeField] private PuzzlePiece _piecePrefab;
+    [SerializeField] private List<PuzzlePiece> _piecePrefabs;
     [SerializeField] private Transform _slotParent, _pieceParent;
 
     private void Start()
@@ -15,18 +15,18 @@ public class PuzzleManager : MonoBehaviour
 
     void Spawn()
     {
-        if (_slotPrefabs.Count != 0)
+        if (_slotPrefabs.Count != 0 && _piecePrefabs.Count != 0 && _slotPrefabs.Count == _piecePrefabs.Count)
         {
             for (int i = 0; i < _slotPrefabs.Count; i++)
             {
                 var spawnedSlot = Instantiate(_slotPrefabs[i], _slotParent.GetChild(i).position, Quaternion.identity);
-                var spawnedPiece = Instantiate(_piecePrefab, _pieceParent.GetChild(i).position, Quaternion.identity);
+                var spawnedPiece = Instantiate(_piecePrefabs[i], _pieceParent.GetChild(i).position, Quaternion.identity);
                 spawnedPiece.Init(spawnedSlot);
             }
         }
         else
         {
-            Debug.LogWarning("No slot prefabs found in the list.");
+            Debug.LogWarning("Slot prefabs or piece prefabs lists are either empty or have different sizes.");
         }
     }
 }
